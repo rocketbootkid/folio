58bf373443593c8add32e98c3e93b4a3108655b5
Richard Paterson <rocketbootkid@gmail.com>
2016-07-10 23:19:03 +0100

    First stuff

---
+++ bin/functions.php
@@ -0,0 +1,110 @@
+<?php
+
+$root = "../db";
+
+
+# -------------------------------------------------------------------------------------------------------
+#											S C H E M A   F U N C T I O N S
+# -------------------------------------------------------------------------------------------------------
+
+function createSchema($schema_name) {
+	
+	
+	
+	
+	
+}
+
+function backupSchema($schema_name) {
+	
+	
+	
+	
+	
+}
+
+function dropSchema($schema_name) {
+	
+	
+	
+}
+
+function listSchema() {
+	
+	
+	
+	
+}
+
+# -------------------------------------------------------------------------------------------------------
+#										D A T A B A S E   F U N C T I O N S
+# -------------------------------------------------------------------------------------------------------
+
+function createDatabase($database_name, $schema_name) {
+	
+	
+	
+	
+}
+
+function deleteDatabase($database_name, $schema_name) {
+	
+	
+	
+	
+}
+
+function listDatabase($schema_name) {
+	
+	
+	
+	
+}
+
+# -------------------------------------------------------------------------------------------------------
+#										T A B L E   F U N C T I O N S
+# -------------------------------------------------------------------------------------------------------
+
+function createTable() {
+	
+	
+	
+	
+}
+
+function deleteTable() {
+	
+	
+	
+	
+}
+
+function addRecord() {
+	
+	
+	
+	
+}
+
+function editRecord() {
+	
+	
+	
+	
+}
+
+function deleteRecord() {
+	
+	
+	
+	
+	
+}
+
+
+
+
+
+
+
+?>
